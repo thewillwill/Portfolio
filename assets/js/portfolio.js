0c94e13643640beaac3bf565c02c4ff8e2e85dad
William Brooks <brooks.william@gmail.com>
2017-11-23 10:34:12 -0800

    added jquery height adjustments for div

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -10,19 +10,27 @@ $(function() {
         } else {
             $('#arrow').fadeIn();
             $('#navigation').fadeOut();
-
+            $("content-holder").css('margin-top',150);
         }
     });
 })
 
+// Set the heights of the Bio Divs
 
-//scroll down from top arrow
+var biggestHeight = 0;
+// Loop through elements children to find & set the biggest height
+$(".bio-text").each(function(){
+ // If this elements height is bigger than the biggestHeight
+ if ($(this).height() > biggestHeight ) {
+   // Set the biggestHeight to this Height
+   biggestHeight = $(this).height();
+ }
+});
 
-$("#arrow").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#brief-bio").offset().top
-    }, 2000);
-})
+$(".bio-text").height(biggestHeight);
+
+
+// ANIMATE ON HOVER
 
 $("#bio-more").hover(function() {
     $(this).addClass("animated shake");
@@ -32,23 +40,33 @@ $("#bio-less").hover(function() {
     $(this).addClass("animated shake");
 });
 
+// ON CLICK LISTENERS
 
+//scroll down from top arrow
+$("#arrow").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#brief-bio").offset().top
+    }, 2000);
+})
+
+
+//BIO More and Less
 $("#bio-more").click(function() {
-    $("#bio-text-1").addClass("animated rollOut").animate({ opacity: 0 }, {
+    $("#bio-text-1").removeClass("rollInRight").addClass("animated rollOutRight").animate({ opacity: 0 }, {
         duration: 500,
         complete: function() {
             $("#bio-text-1").addClass("hidden");
-            $("#bio-text-2").removeClass("hidden").addClass("animated rollIn");
+            $("#bio-text-2").removeClass("hidden").addClass("animated rollInLeft");
         }
     });
 });
 
 $("#bio-less").click(function() {
-    $("#bio-text-2").addClass("animated rollOut").animate({ opacity: 0 }, {
+    $("#bio-text-2").removeClass("rollInLeft").addClass("animated rollOutLeft").animate({ opacity: 0 }, {
         duration: 500,
         complete: function() {
             $("#bio-text-2").addClass("hidden");
-            $("#bio-text-1").removeClass("hidden").addClass("animated rollIn");
+            $("#bio-text-1").removeClass("hidden rollOutRight").addClass("animated rollInRight");
         }
     });
 });
